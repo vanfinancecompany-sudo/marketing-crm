b1fd8f8118113d9be9436be55e89b5c7ee8afd37
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-04-17 07:59:09 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,23 @@
-import ControlCentrePage from "./pages/ControlCentrePage.jsx";
+import Sidebar from "./components/Sidebar";
 
 export default function App() {
-  return <ControlCentrePage />;
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <div style={{ padding: 20 }}>
+        <h1>Control Centre</h1>
+        <p>This is the Control Centre app</p>
+
+        <a href="https://crm-b5po.vercel.app">
+          Go to Main CRM
+        </a>
+
+        <br /><br />
+
+        <a href="https://crm-b5po.vercel.app">
+          Go to Marketing CRM
+        </a>
+      </div>
+    </div>
+  );
 }
