a75f60e4b7b91d1cdd5e229a7c217beabf9dcfb1
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-05-10 23:58:06 +0100

    Hydrate synced posting hidden state on app start

--- main.jsx
+++ main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import { hydratePostingHiddenState, installPostingStateLocalStorageSync } from "./services/postingStateSync.js";
+
+installPostingStateLocalStorageSync();
+void hydratePostingHiddenState();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
