e45e5cc087d929cd4a15af0e0e4cf7cd51bdfc59
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-06-15 20:27:40 +0100

    Load YouTube Generator description auto files

--- main.jsx
+++ main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles.css";
 import "./utils/overnightAutoRefreshPause.js";
 import "./utils/postingVisibilityStateAutoSync.js";
+import "./utils/youtubeDescriptionAutoFiles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
