bc099530510351ea1dcf2a0ba7db6c4a0bdb9e26
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-08-05 09:52:47 +0100

    Gate Marketing CRM behind single active tab lock

--- main.jsx
+++ main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SingleActiveTabGate from "./components/SingleActiveTabGate.jsx";
 import "./styles.css";
 import "./utils/overnightAutoRefreshPause.js";
 import "./utils/postingVisibilityStateAutoSync.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SingleActiveTabGate>
+      <App />
+    </SingleActiveTabGate>
   </React.StrictMode>
 );
