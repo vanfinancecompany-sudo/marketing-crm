9ac9cd97df470de12227b27ad42624a7cb532583
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-08-17 12:21:31 +0100

    Add no-lock Marketing Centre AI platform route

---
+++ api/marketing-centre-ai-platform.js
@@ -0,0 +1,4 @@
+import aiMarketingPlatformHandler from "./marketing-ai-platform.js";
+import { withMarketingCentreNoLock } from "../lib/marketingCentreNoLock.js";
+
+export default withMarketingCentreNoLock(aiMarketingPlatformHandler);
