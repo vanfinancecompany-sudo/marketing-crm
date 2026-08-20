63e16cca00731d2f04bc23e2c6dbbda3a2b4c153
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-08-20 20:30:08 +0100

    Route Knowledge Hub editorial review through dedicated Sol model policy

--- api/knowledge-hub-ui-editorial-engine.js
+++ api/knowledge-hub-ui-editorial-engine.js
@@ -1,4 +1,7 @@
 import handler from "./marketing-editorial-engine.js";
 import { withKnowledgeHubNoLock } from "../lib/knowledgeHubNoLock.js";
+import { applyKnowledgeModelOverride } from "../lib/priorityAiModelPolicy.js";
+
+applyKnowledgeModelOverride(process.env, "review");
 
 export default withKnowledgeHubNoLock(handler);
