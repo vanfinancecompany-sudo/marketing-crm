fa6b6b555beee50553507c293c265c271a47cb2f
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-08-27 11:58:32 +0100

    Route Stock Watch Wix pricing through Marketing Centre no-lock

---
+++ api/marketing-centre-vansco-wix-price.js
@@ -0,0 +1,4 @@
+import vanscoWixPriceHandler from "./vansco-wix-price.js";
+import { withMarketingCentreNoLock } from "../lib/marketingCentreNoLock.js";
+
+export default withMarketingCentreNoLock(vanscoWixPriceHandler);
