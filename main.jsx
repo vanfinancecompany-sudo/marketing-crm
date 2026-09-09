82f468887a2649ab4cbfe3173a3f45481e7e74b3
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-09-09 15:28:38 +0100

    Use polished Stock Control Centre layout

--- main.jsx
+++ main.jsx
@@ -7,7 +7,7 @@ import AutomationHealthCentre from "./components/AutomationHealthCentre.jsx";
 import StockReconciliationAgent from "./components/StockReconciliationAgent.jsx";
 import "./styles.css";
 import "./styles/stock-control-centre.css";
-import "./styles/stock-control-centre-layout-fix.css";
+import "./styles/stock-control-centre-polish.css";
 import "./utils/overnightAutoRefreshPause.js";
 import "./utils/postingVisibilityStateAutoSync.js";
 import "./utils/vanscoWixPriceHelper.js";
