cbd7555d7ce9865c85fae0d5815ea09c5af6d7e4
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-09-10 19:12:54 +0100

    Run final controlled publish regression suite in build gate

--- tests/dealerkit-wix-manual-media-preview.test.js
+++ tests/dealerkit-wix-manual-media-preview.test.js
@@ -2,6 +2,7 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { readFile } from "node:fs/promises";
 import { buildDealerKitWixManualMediaReadiness } from "../lib/dealerKitWixManualMediaReadiness.js";
+import "./dealerkit-controlled-publish.test.js";
 
 const root = new URL("../", import.meta.url);
 
