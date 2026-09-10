f81effeda76b92de77bddfd80a27518064054644
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-09-10 15:06:21 +0100

    Wire DealerKit Wix publish preview into CRM

--- main.jsx
+++ main.jsx
@@ -10,12 +10,14 @@ import "./styles/stock-control-centre.css";
 import "./styles/stock-control-centre-polish.css";
 import "./styles/dealerkit-stock-control-preview.css";
 import "./styles/dealerkit-review-workspace.css";
+import "./styles/dealerkit-wix-publish-preview.css";
 import "./utils/overnightAutoRefreshPause.js";
 import "./utils/postingVisibilityStateAutoSync.js";
 import "./utils/vanscoWixPriceHelper.js";
 import "./utils/stockControlCentreUi.js";
 import "./utils/dealerKitStockControlPreview.js";
 import "./utils/dealerKitReviewWorkspace.js";
+import "./utils/dealerKitWixPublishPreview.js";
 
 const ACTIVE_INTEGRATIONS = Object.freeze({
   liveStatus: "/buffer-live-status.js",
