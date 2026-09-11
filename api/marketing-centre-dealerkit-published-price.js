fb17dc568c99320e3bedd0539a05be760c7a1efc
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-09-11 01:13:54 +0100

    Expose controlled published price updater to Marketing CRM

---
+++ api/marketing-centre-dealerkit-published-price.js
@@ -0,0 +1,4 @@
+import dealerKitPublishedPriceHandler from "./dealerkit-published-price.js";
+import { withMarketingCentreNoLock } from "../lib/marketingCentreNoLock.js";
+
+export default withMarketingCentreNoLock(dealerKitPublishedPriceHandler);
